d41df64d2799a842b7ff3388ab8935af4b7b12e6
Your Name <sithijakavee120@gmail.com>
2024-11-23 07:28:45 +0530

    wtf

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,26 @@
 "use client";
+
 import { useEffect, useState } from "react";
 
+type TypeLocation = {
+  city: string;
+  country: string;
+  ip: string;
+  loc: string;
+  org: string;
+  postal: string;
+  readme: string;
+  region: string;
+  timezone: string;
+};
+
 export default function Home() {
-  const [ip, setIp] = useState();
+  const [ip, setIp] = useState<TypeLocation>();
   useEffect(() => {
     const getClientLocation = async () => {
       const res = await fetch("https://ipinfo.io/json");
       const locationData = await res.json();
-      setIp(locationData.ip);
+      setIp(locationData);
 
       console.log(locationData);
     };
@@ -15,5 +28,11 @@ export default function Home() {
     getClientLocation();
   }, []);
 
-  return <h1 className="text-3xl font-extrabold">{ip}</h1>;
+  return (
+    <>
+      <h1 className="text-2xl font-extrabold">{ip?.ip}</h1>
+      <span className="text-xl font-semibold">{ip?.city}</span>
+      <span className="text-xl font-semibold">{ip?.country} </span>
+    </>
+  );
 }
