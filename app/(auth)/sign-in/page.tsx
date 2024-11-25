96407d0b8dd999b5232e086b35dc8fd6f0c96583
Your Name <sithijakavee120@gmail.com>
2024-11-26 01:59:46 +0530

    v1.02

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -27,7 +27,7 @@ const page = () => {
     <div className="w-full h-screen flex flex-col gap-2 justify-center items-center">
       <div className="flex gap-1 items-end mb-2">
         <h1 className="text-3xl font-bold">AuthN</h1>
-        <h1 className="text-md font-bold">Sign Up</h1>
+        <h1 className="text-md font-bold">Sign In</h1>
       </div>
 
       <SigninForm />
